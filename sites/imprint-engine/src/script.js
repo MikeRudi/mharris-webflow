399c029f774660e101201e3857bc3e132c3b3b7f
mike <mikerudyharris@gmail.com>
2026-08-06 12:46:02 +0200

    Slow filter line animation

--- sites/imprint-engine/src/script.js
+++ sites/imprint-engine/src/script.js
@@ -160,8 +160,8 @@ function filterOne() {
     exitStagger: 0.035,
     enterStagger: 0.05,
     itemStagger: 0.03,
-    lineExitDuration: 0.3,
-    lineEnterDuration: 0.45,
+    lineExitDuration: 0.9,
+    lineEnterDuration: 1.35,
     heightDuration: 0.55,
   };
 
